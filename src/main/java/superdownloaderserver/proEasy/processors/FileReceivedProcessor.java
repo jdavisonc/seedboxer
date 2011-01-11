bd8e0fd34d1eec2af5d27518e91ad996f4dcbd55
Jorge Davison <jdavisonc@gmail.com>
2011-01-11 13:19:10 +0000

    Set properties and create structure of processors.

---
+++ src/main/java/superdownloaderserver/proEasy/processors/FileReceivedProcessor.java
@@ -0,0 +1,40 @@
+
+package superdownloaderserver.proEasy.processors;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+
+/**
+ * @author jdavison
+ *
+ */
+public class FileReceivedProcessor implements Processor {
+
+	private Pattern pattern = Pattern.compile("");
+	
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		Message msg = exchange.getIn();
+		
+		//String origFileName = (String) msg.getHeader(Exchange.FILE_NAME_ONLY);
+		//File f = new File((String) msg.getHeader(Exchange.FILE_PATH));
+		//String fileFolder = (String) msg.getHeader(Exchange.FILE_NAME);
+		
+		/*Matcher m = pattern.matcher((String) msg.getHeader(Exchange.FILE_NAME));
+		if (m.matches()) {
+			String userName = m.group(1);
+			msg.setHeader("USER_NAME", userName);
+		}else
+			throw new Exception("The file doesn't compile with the pattern.");*/
+		
+		//TODO: Set header for FTP component to upload
+		
+		//TODO: Set header for SMTP component to send notification
+
+	}
+
+}
