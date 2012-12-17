79017d1c0defa47c4878e54b8db6ea0734c7a340
Jorge Davison <jdavisonc@gmail.com>
2012-12-17 21:48:17 -0200

    Update sql/migration.sql

--- sql/migration.sql
+++ sql/migration.sql
@@ -1,2 +1,3 @@
 update `proeasy`.`users_configs` set name = 'NotificationGCM' where name = 'NotificationC2dm';
 update `proeasy`.`users_configs` set name = 'NotificationGCMRegistrationId' where name = 'NotificationC2dmRegistrationId';
+ALTER TABLE `proeasy`.`users` ADD `status` varchar(10) DEFAULT 'STARTED' AFTER `password`;
