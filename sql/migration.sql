04756884f79ec681a8eb3f80209bb2bbe66f37a3
Jorge Davison <jdavisonc@gmail.com>
2013-01-11 21:10:20 -0200

    - Remove extra configuration property when user wants to set a email notification. Now SeedBoxer only requires 'NotificationEmail' and its value is the final email, if this property is not set for the user, then no email notification will be sent. - Improve logging in camel routes. - Improve unexpected error handling in camel routes.

--- sql/migration.sql
+++ sql/migration.sql
@@ -1,3 +1,2 @@
-update `proeasy`.`users_configs` set name = 'NotificationGCM' where name = 'NotificationC2dm';
-update `proeasy`.`users_configs` set name = 'NotificationGCMRegistrationId' where name = 'NotificationC2dmRegistrationId';
-ALTER TABLE `proeasy`.`users` ADD `status` varchar(10) DEFAULT 'STARTED' AFTER `password`;
+delete from `seedboxer`.`users_configs` where name = 'NotificationEmail';
+update `seedboxer`.`users_configs` set name = 'NotificationEmail' where name = 'NotificationEmailEmail';
--- src/main/java/net/seedboxer/seedboxer/mule/processor/notification/EmailNotification.java
+++ src/main/java/net/seedboxer/seedboxer/mule/processor/notification/EmailNotification.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * EmailNotification.java
- * 
+ *
  * Copyright (c) 2012 SeedBoxer Team.
- * 
+ *
  * This file is part of SeedBoxer.
- * 
+ *
  * SeedBoxer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * SeedBoxer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with SeedBoxer.  If not, see <http ://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -89,7 +89,7 @@ public class EmailNotification extends Notification {
 	 * @param params
 	 */
 	private void setEmailProperties(Message msg, String subjectTpl, String bodyTpl, Map<String, Object> params) {
-		String email = (String) msg.getHeader(net.seedboxer.seedboxer.core.domain.Configuration.NOTIFICATION_EMAIL_EMAIL);
+		String email = (String) msg.getHeader(net.seedboxer.seedboxer.core.domain.Configuration.NOTIFICATION_EMAIL);
 		msg.setHeader(TO_HEADER, email);
 		msg.setHeader(SUBJECT_HEADER, getProcessedTemplate(subjectTpl, params));
 		msg.setBody(getProcessedTemplate(bodyTpl, params));
