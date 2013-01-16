b630daed148cbe9b538ada228f841c57e4f06ddd
Jorge Davison <jdavisonc@gmail.com>
2013-01-15 23:21:53 -0200

    - Externalize endpoints uri in sources camel context - Refactor in RSS Feeds consumer

--- src/main/java/net/seedboxer/seedboxer/sources/processors/rss/RssConsumer.java
+++ src/main/java/net/seedboxer/seedboxer/sources/processors/rss/RssConsumer.java
@@ -1,25 +1,25 @@
 /*******************************************************************************
  * RssConsumer.java
- * 
+ *
  * Copyright (c) 2012 SeedBoxer Team.
- * 
+ *
  * This file is part of SeedBoxer.
- * 
+ *
  * SeedBoxer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * SeedBoxer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with SeedBoxer.  If not, see <http ://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-package net.seedboxer.seedboxer.sources.processors;
+package net.seedboxer.seedboxer.sources.processors.rss;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,7 +34,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndFeed;
 
 /**
  *
@@ -49,8 +48,7 @@ public class RssConsumer implements Processor{
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		Message msg = exchange.getIn();
-		SyndFeed feed = (SyndFeed) msg.getBody();
-		List<SyndEntry > entries = feed.getEntries();
+		List<SyndEntry > entries = (List<SyndEntry>) msg.getBody();
 		List<MatchableItem> items = new ArrayList<MatchableItem>();
 
 		LOGGER.debug("Incoming rss entries: {}", entries.size());
--- src/main/java/net/seedboxer/seedboxer/sources/routes/RouteBuilder.java
+++ src/main/java/net/seedboxer/seedboxer/sources/routes/RouteBuilder.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * RouteBuilder.java
- * 
+ *
  * Copyright (c) 2012 SeedBoxer Team.
- * 
+ *
  * This file is part of SeedBoxer.
- * 
+ *
  * SeedBoxer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * SeedBoxer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with SeedBoxer.  If not, see <http ://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -25,10 +25,10 @@ import java.util.List;
 
 import net.seedboxer.seedboxer.core.domain.RssFeed;
 import net.seedboxer.seedboxer.core.logic.FeedsManager;
+import net.seedboxer.seedboxer.sources.processors.rss.filter.RssDateFilter;
 
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -39,22 +39,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class RouteBuilder extends SpringRouteBuilder {
 
+	private static final String MERGE_FEEDS_ENDPOINT = "direct:mergeFeeds";
+	private static final String RSS_TIMER_ENDPOINT = "rssTimer";
+
 	@Autowired
 	private FeedsManager feedsManager;
-
-	@Value(value="${rssPollPeriod}")
-	private String rssPollPeriod;
 
 	@Override
 	public void configure() throws Exception {
 		List<RssFeed> feeds = feedsManager.getAllFeeds();
 		for(RssFeed feed : feeds){
-			from(String.format("timer://%s?fixedRate=true&delay=0&period=%s",feed.getId(),rssPollPeriod))
+			configureFeedRoute(feed);
+		}
+	}
+
+	private void configureFeedRoute(RssFeed feed) {
+		from(RSS_TIMER_ENDPOINT)
 			.to(feed.getUrl())
 			.unmarshal()
 			.rss()
-			.split().body()
-			.to("direct:mergeFeeds");
-		}
+			.split().simple("body.entries")
+			.filter().method(RssDateFilter.class, "isValidEntry")
+		.to(MERGE_FEEDS_ENDPOINT);
 	}
 }
