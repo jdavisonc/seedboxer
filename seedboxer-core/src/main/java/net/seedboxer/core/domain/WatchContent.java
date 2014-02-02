69e35da0068a192c990a5d52898166a46ab81101
jdavisonc <jdavisonc@gmail.com>
2014-02-02 19:29:47 -0200

    Set fields of class.

--- seedboxer-core/src/main/java/net/seedboxer/core/domain/WatchContent.java
+++ seedboxer-core/src/main/java/net/seedboxer/core/domain/WatchContent.java
@@ -21,6 +21,11 @@
 package net.seedboxer.core.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.google.common.base.Objects;
 
 /**
  * @author Jorge Davison (jdavisonc)
@@ -29,8 +34,55 @@ import javax.persistence.Entity;
 @Entity
 public class WatchContent {
 	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "content_id")
 	private Content content;
 	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "user_id")
 	private User user;
 
+	public Content getContent() {
+		return content;
+	}
+
+	public void setContent(Content content) {
+		this.content = content;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(content, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WatchContent other = (WatchContent) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
 }
